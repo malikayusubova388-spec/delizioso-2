b3200cef2fa0a704fed892b2bb68e19c9cc2c533
Sherzod Sheraliyev <sheraliyevs147@gmail.com>
2026-03-03 11:08:06 +0500

    header done

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        primery: '#FF8A00',
+        secondary: '#3FA72F',
+        thirdstage: '#311F09'
+      }
+    },
+    container: {
+      center: true,
+      padding: '15px',
+      sm: '1130px'
+    }
+  },
+  plugins: [],
+}
+
